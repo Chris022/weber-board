307efbbd7c42542de86f045cbdfd35ea70bfb169
Chris022 <christoph02.cw@gmail.com>
2021-02-25 22:12:56 +0100

    :sparkles: add delte Button

--- src/client/input.js
+++ src/client/input.js
@@ -37,8 +37,12 @@ function onMouseDown(e) {
   points = []
   button = e.button;
 
+  if(document.getElementById("writeReadButton").innerHTML != "Write"){
+    button = 2;
+  }
+
   if(button == 2){
-    checkCollission([getXY(e.clientX,e.clientY)],getCurrentState())
+    checkCollission(getXY(e.clientX,e.clientY),getCurrentState())
   }
   return false;
 }
@@ -66,11 +70,14 @@ function onTouchMove(e) {
       //handle zoom
       var newDist = Math.sqrt((touch.pageX-touch1.pageX)**2 + (touch.pageY-touch1.pageY)**2)
       addScaleTouch((newDist-lastDist)/100);
-
-
       lastTouchX = touch.pageX;
       lastTouchY = touch.pageY;
       lastDist = newDist;
+      break;
+    case 2:
+      var touch = e.touches[0];
+      console.log("here")
+      checkCollission(getXY(touch.pageX,touch.pageY),getCurrentState())
       break;
   }
 }
@@ -79,7 +86,12 @@ function onTouchStart(e) {
   matrix = getInvertedTransformMatrix();
   switch (e.touches.length) {
     case 1:
-      button = 0;
+      if(document.getElementById("writeReadButton").innerHTML == "Write"){
+        button = 0;
+      }else{
+        button = 2;
+      }
+
       points = []
       break;
     case 2: 
@@ -92,7 +104,7 @@ function onTouchStart(e) {
 }
 
 function onTouchEnd(e){
-  if(button = 1){
+  if(button == 0){
     draw({"points":points,"color":getColor()});
     lastDrawing = {"points":points,"color":getColor()};
   }
